476062957006fed11ad8e57ea5d8bc8232645f0f
EarthNatchanon <nnatchanon187@gmail.com>
2025-07-27 22:10:52 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import ReHabitNavbar from './components/ReHabitNavbar';
-import ReHabitNavbarGuest from './components/ReHabitNavbarGuest';
+import ReHabitNavbar from './components/ReHabitNavbar';
+// import ReHabitNavbarGuest from './components/ReHabitNavbarGuest';
 import MediaPipefull from './page/MediaPipefull';
 import Home from './page/Home';
-import Mediapipe2 from './page/Mediapipe2';
 import EditProfile from './page/EditProfile';
 import OfficeSyndromePage from './page/OfficeSyndrome';
 import ContactUs from './page/ContactUs';
@@ -15,14 +14,13 @@ import MediaPipefullDemo from './page/MediaPipefulldemo'
 function App() {
   return (
     <Router>
-      {/* <ReHabitNavbar /> */}
-      <ReHabitNavbarGuest />
+      <ReHabitNavbar />
+      {/* <ReHabitNavbarGuest /> */}
 
       <Routes>
 
         <Route path="/" element={<Home />} />
         <Route path="/MediaPipefull" element={<MediaPipefull />} />
-        <Route path="/Mediapipe2" element={<Mediapipe2 />} />
         <Route path="/edit-profile" element={<EditProfile />} />  
         <Route path="/office-syndrome" element={<OfficeSyndromePage />} />
         <Route path="/contact-us" element={<ContactUs />} />
